303d268c31ca3280fa50f4f593c4ba841dfbd3a3
MahdieRrad <93393815+MahdieRrad@users.noreply.github.com>
2021-10-29 23:35:31 +0330

    Update Exr5.py

--- imgeP-1/Exr5.py
+++ imgeP-1/Exr5.py
@@ -1,9 +1,7 @@
 import cv2
 
 imge = cv2.imread('5.jpg',0)
-imge = cv2.resize(imge,(650,500))
-cv2.line(imge,(-5,150),
-             (150,-5),
-             (10,10,10),25)
-cv2.imshow('TonyStrak Dead',imge)
+imge = cv2.resize(imge,(500,500))
+cv2.line(imge,(-5,150),(150,-5),(10,10,10),20)
+cv2.imshow('TonyStark Dead',imge)
 cv2.waitKey()
